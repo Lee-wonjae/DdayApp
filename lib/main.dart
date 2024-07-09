956c49d18802523c63884d5dbc1d840e88cc590e
jerry <lwj0173@gmail.com>
2024-07-09 23:43:12 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:you_and_i/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+        textTheme: TextTheme(
+          headlineLarge: TextStyle(
+            color: Colors.white,
+            fontSize: 80.0,
+            fontWeight: FontWeight.w700,
+            fontFamily: 'parisienne',
+          ),
+          headlineMedium: TextStyle(
+            color: Colors.white,
+            fontSize:50.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyLarge: TextStyle(
+            color: Colors.white,
+            fontSize:30.0,
+          ),
+          bodyMedium: TextStyle(
+            color:Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
+      home:HomeScreen(),
+    )
+  );
+}
